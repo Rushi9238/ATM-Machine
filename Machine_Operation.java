0f86139af9ca7953af33fee6c6c24da32a3170ab
Rushi9238 <113586690+Rushi9238@users.noreply.github.com>
2022-09-15 15:57:40 +0530

    Add files via upload

---
+++ Machine_Operation.java
@@ -0,0 +1,9 @@
+package Project;
+
+public interface Machine_Operation {
+
+	 public void viewBalance();
+	    public void withdrawAmount(double withdrawAmount);
+	    public void depositAmount(double depositAmount );
+	    public  void viewMiniStatement();
+}
